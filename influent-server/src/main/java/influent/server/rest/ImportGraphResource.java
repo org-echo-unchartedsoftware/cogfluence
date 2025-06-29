4985c5f3131e8a6998856140c4fa74be008a03ea
drzo <d@rzo.io>
2025-06-29 14:34:15 +0200

    Initialize copilot/fix-97

---
+++ influent-server/src/main/java/influent/server/rest/ImportGraphResource.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2013-2016 Uncharted Software Inc.
+ *
+ *  Property of Uncharted(TM), formerly Oculus Info Inc.
+ *  https://uncharted.software/
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package influent.server.rest;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import influent.server.spi.ImportDataService;
+import jakarta.xml.bind.JAXBException;
+import java.util.List;
+import oculus.aperture.common.rest.ApertureServerResource;
+import oculus.aperture.spi.store.ConflictException;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.restlet.data.MediaType;
+import org.restlet.data.Status;
+import org.restlet.ext.fileupload.RestletFileUpload;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.resource.Post;
+import org.restlet.resource.ResourceException;
+
+public class ImportGraphResource extends ApertureServerResource {
+
+  private final ImportDataService service;
+
+  @Inject
+  public ImportGraphResource(
+      ImportDataService service, @Named("influent.charts.maxage") Integer maxCacheAge) {
+    this.service = service;
+  }
+
+  @Post
+  public StringRepresentation getCapturedImage(Representation entity) throws ResourceException {
+
+    if (entity == null || !MediaType.MULTIPART_FORM_DATA.equals(entity.getMediaType(), true))
+      return null;
+
+    DiskFileItemFactory factory = new DiskFileItemFactory();
+    factory.setSizeThreshold(1000240);
+    RestletFileUpload upload = new RestletFileUpload(factory);
+
+    try {
+      List<FileItem> items = upload.parseRequest(getRequest());
+      String xmlData = new String(items.get(0).get());
+
+      JSONObject influentState = executeTask(xmlData); // hit the service to get the JSONObject
+
+      if (influentState == null) {
+        throw new ResourceException(
+            Status.SERVER_ERROR_INTERNAL,
+            "XML processing failed to complete for an unknown reason.");
+      }
+
+      return new StringRepresentation(influentState.toString(), MediaType.TEXT_PLAIN);
+
+    } catch (Exception e) {
+      throw new ResourceException(
+          Status.CLIENT_ERROR_BAD_REQUEST,
+          "Unable to create JSON object from supplied options string",
+          e);
+    }
+  }
+
+  private JSONObject executeTask(String xmlData)
+      throws ConflictException, JSONException, JAXBException {
+    return service.importFromXML(xmlData);
+  }
+}
---
+++ influent-server/src/main/java/influent/server/spi/impl/graphml/JAXBXMLHandler.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2013-2016 Uncharted Software Inc.
+ *
+ *  Property of Uncharted(TM), formerly Oculus Info Inc.
+ *  https://uncharted.software/
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package influent.server.spi.impl.graphml;
+
+import jakarta.xml.bind.ValidationEventHandler;
+import jakarta.xml.bind.annotation.DomHandler;
+import java.io.StringReader;
+import java.io.StringWriter;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+/**
+ * Simple handler to marshal/unmarshal XML text in JAXB. This is necessary to ensure the XML text is
+ * actually output as XML, as opposed to having &lt; and &gt; replacements everywhere.
+ *
+ * @author pmoore
+ */
+public class JAXBXMLHandler implements DomHandler<String, StreamResult> {
+  private static final String JSON_START_TAG = "<json>";
+  private static final String JSON_END_TAG = "</json>";
+
+  private StringWriter xmlWriter = new StringWriter();
+
+  public StreamResult createUnmarshaller(ValidationEventHandler errorHandler) {
+    return new StreamResult(xmlWriter);
+  }
+
+  public String getElement(StreamResult rt) {
+    String xml = rt.getWriter().toString();
+    int beginIndex = xml.lastIndexOf(JSON_START_TAG) + JSON_START_TAG.length();
+    int endIndex = xml.lastIndexOf(JSON_END_TAG);
+    return xml.substring(beginIndex, endIndex);
+  }
+
+  public Source marshal(String n, ValidationEventHandler errorHandler) {
+    try {
+      String xml = JSON_START_TAG + n.trim() + JSON_END_TAG;
+      StringReader xmlReader = new StringReader(xml);
+      return new StreamSource(xmlReader);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
